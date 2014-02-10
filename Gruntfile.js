d572db657826838b9d66a03510970b9c2077dae5
Patrick Metcalfe <git@patrickmetcalfe.com>
2014-02-10 16:22:29 -0600

    Finally

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+    
+	
+	grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}, sass/modules/*.{sass, scss}'],
+                tasks: ['compass:dev']
+            },
+            livereload: {
+                files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+		compass: {
+		      dev: {
+		        options: {
+		          config: 'config.rb',
+				  quiet: true,
+		          force: true,
+		        }
+		      }
+		    },
+		'bower-install': {
+
+			target: {
+
+				src: ['index.html'],
+
+			}
+		}	
+	});
+    grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bower-install');
+};
